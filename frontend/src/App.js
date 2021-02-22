3cd30adea7e42bd10879f0a465d7b250c4a90e41
Duupec <71795400+Duupec@users.noreply.github.com>
2021-02-22 11:32:25 -0500

    creando numero de reviews y estrellas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -6,42 +8,21 @@ function App() {
       <header className="row">
         
         <div>
-          <a className="brand" href="/">Proyecto</a> 
+          <a className="brand" href="/">Luz</a> 
         </div>
         
         <div>
           <a href="/cart">Carrito</a>
           <a href="/signin">Sign In</a>
         </div>
-
       </header>
-      
       <main>
        <div className="row center">
          {
            data.products.map(product =>( /* creo que product es simplemente un objeto que hereda llamando a data.js, a productos especficiamente para hcaer dinamico el cambio de informacion*/
-            <div key={product.__id}className="card">
-            <a href={`/product/${product.__id}`}> 
-                <img className="medium" src={product.image} alt={product.name}/> 
-            </a>
-
-            <div className="card-body">
-                <a href={`/product/${product.__id}`}> 
-                    <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                 <span><i className="fa fa-star"></i></span>
-                 <span><i className="fa fa-star"></i></span>
-                 <span><i className="fa fa-star"></i></span>
-                 <span><i className="fa fa-star"></i></span>
-                 <span><i className="fa fa-star"></i></span>
-                </div>
-                <div className="price">${product.price}</div>
-            </div>
-        </div>
+            <Product key={product.__id} product={product}> </Product>
            ))
          }
-           
        </div>
       </main>
       <footer className="row center">All right reserved</footer>
