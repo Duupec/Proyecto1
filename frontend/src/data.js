75a5bcce2303fb31c7827d105c3f6d8264ca1a47
Duupec <71795400+Duupec@users.noreply.github.com>
2021-02-21 20:21:58 -0500

    init

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data= {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'Producto de alta calidad'
+        },
+        {
+            _id:'2',
+            name:'Polo adidas',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price:322,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'Producto de alta calidad'
+        },
+        {
+            _id:'3',
+            name:'Polo Lacoste',
+            category: 'Shirts',
+            image:'/images/p3.jpg',
+            price:135,
+            brand:'Lacoste',
+            rating:4.5,
+            numReviews:9,
+            description:'Producto de alta calidad'
+        },
+        {
+            _id:'4',
+            name:'Pantalon nike',
+            category: 'Pants',
+            image:'/images/p4.jpg',
+            price:176,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:11,
+            description:'Producto de alta calidad'
+        },
+        {
+            _id:'5',
+            name:'Pantalon puma',
+            category: 'Pants',
+            image:'/images/p5.jpg',
+            price:55,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:13,
+            description:'Producto de alta calidad'
+        },
+        {
+            _id:'6',
+            name:'Pantalon adidas',
+            category: 'Pantss',
+            image:'/images/p6.jpg',
+            price:154,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:15,
+            description:'Producto de alta calidad'
+        },
+    ],
+};
+
+export default data
